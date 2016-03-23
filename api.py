2fb06f932a94bb3ef61c733cce84fc4d9be9d317
Diogo Kersting <diogovk@gmail.com>
2016-03-23 13:41:25 -0300

    return initial board on new game

--- api.py
+++ api.py
@@ -5,20 +5,33 @@ from protorpc import message_types
 
 
 
-class StringMessage(messages.Message):
-        string = messages.StringField(1)
+class BoardMessage(messages.Message):
+    board = messages.StringField(1, repeated=True)
+
+INITIAL_BOARD = BoardMessage(board=[
+               '  ***  ',
+               '  ***  ',
+               '*******',
+               '***o***',
+               '*******',
+               '  ***  ',
+               '  ***  '
+               ])
+
+class UserMessage(messages.Message):
+    user = messages.StringField(1, required=True)
 
 @endpoints.api(name='peg_solitarie', version='v1')
 class PegSolitarieAPI(remote.Service):
     " Peg Solitararie Game API"
 
-    @endpoints.method(request_message=message_types.VoidMessage,
-                      response_message=StringMessage,
-                      path="hello",
-                      name="hello",
+    @endpoints.method(request_message=UserMessage,
+                      response_message=BoardMessage,
+                      path="new_game",
+                      name="new_game",
                       http_method='GET')
-    def hello(self, request):
-        return StringMessage(string="superdood")
+    def new_game(self, request):
+        return INITIAL_BOARD
 
 api = endpoints.api_server([PegSolitarieAPI])
 
