e54f23450fdf96ae9ae8884465df7b68600b38a0
Diogo Kersting <diogovk@gmail.com>
2016-03-24 17:46:13 -0300

    new_game: Creates and saves a new game

--- api.py
+++ api.py
@@ -2,23 +2,10 @@ import endpoints
 from protorpc import remote
 from protorpc import messages
 from protorpc import message_types
-from models import User
+from models import User, Game, GameMessage
 
 class StringMessage(messages.Message):
     message = messages.StringField(1, required=True)
-
-class BoardMessage(messages.Message):
-    board = messages.StringField(1, repeated=True)
-
-INITIAL_BOARD = BoardMessage(board=[
-               '  ***  ',
-               '  ***  ',
-               '*******',
-               '***o***',
-               '*******',
-               '  ***  ',
-               '  ***  '
-               ])
 
 class UserMessage(messages.Message):
     user = messages.StringField(1, required=True)
@@ -32,12 +19,18 @@ class PegSolitarieAPI(remote.Service):
     " Peg Solitararie Game API"
 
     @endpoints.method(request_message=UserMessage,
-                      response_message=BoardMessage,
+                      response_message=GameMessage,
                       path="new_game",
                       name="new_game",
                       http_method='GET')
     def new_game(self, request):
-        return INITIAL_BOARD
+        user = User.query(User.name==request.user).get()
+        if user:
+            game = Game.new_game(user=user.key)
+
+            return game.to_message()
+        else:
+            raise endpoints.NotFoundException("This user doesn't exist")
 
     @endpoints.method(request_message=NewUserMessage,
                       response_message=StringMessage,
@@ -51,7 +44,6 @@ class PegSolitarieAPI(remote.Service):
         user = User(name=request.username, email=request.email)
         user.put()
         return StringMessage(message="User successfully created.")
-
 
 
 api = endpoints.api_server([PegSolitarieAPI])
--- models.py
+++ models.py
@@ -1,6 +1,40 @@
 from google.appengine.ext import ndb
+from protorpc import messages
+
+INITIAL_BOARD = [ '  ***  ',
+                  '  ***  ',
+                  '*******',
+                  '***o***',
+                  '*******',
+                  '  ***  ',
+                  '  ***  '
+                ]
 
 class User(ndb.Model):
     """ User profile """
     name = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
+
+class GameMessage(messages.Message):
+    """ Transferable Game State Information """
+    user = messages.StringField(1)
+    board = messages.StringField(2, repeated=True)
+    game_over = messages.BooleanField(3)
+
+
+class Game(ndb.Model):
+    """ Game Object """
+    user = ndb.KeyProperty(required=True)
+    board = ndb.PickleProperty(required=True)
+    game_over = ndb.BooleanProperty(required=True, default=False)
+
+    @classmethod
+    def new_game(cls, user):
+        game=cls(user=user, board=INITIAL_BOARD)
+        game.put()
+        return game
+
+    def to_message(self):
+        return GameMessage(user=self.user.get().name,
+                           board=self.board,
+                           game_over=self.game_over)
