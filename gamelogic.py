0b57c1c8042d22689d9eb4f7d3cf012f22a93953
Diogo Kersting <diogovk@gmail.com>
2016-03-30 13:43:19 -0300

    PEP8

--- gamelogic.py
+++ gamelogic.py
@@ -1,27 +1,31 @@
 
-BOARD_SIZE=7
-VALID_DIRECTIONS=['u', 'd', 'l', 'r', 'up','down','left','right']
+BOARD_SIZE = 7
+VALID_DIRECTIONS = ['u', 'd', 'l', 'r', 'up', 'down', 'left', 'right']
+
 
 class InvalidMoveExpection(Exception):
     pass
+
 
 def move(current_state, move):
     """
     Moves a peg if a valid move is passed, returning the new game state.
     Throws InvalidMoveExpection otherwize
     """
-    origin_peg_x=letter_to_index(move[0][0])
+    origin_peg_x = letter_to_index(move[0][0])
     # we subtract 1 since externally indexes start at 1 instead of 0
-    origin_peg_y=int(move[0][1])-1
+    origin_peg_y = int(move[0][1])-1
     if not in_bounds(origin_peg_x, origin_peg_y):
         raise InvalidMoveExpection("The origin peg is out of bounds")
-    direction=move[1].lower()
+    direction = move[1].lower()
     if direction not in VALID_DIRECTIONS:
-        raise ValueError("The direction should be"
-                "one of the following options: 'up','down','left','right'")
-    dest_peg=peg_destination(origin_peg_x, origin_peg_y, direction[0])
+        raise ValueError("The direction should be one of the following "
+                         "options: 'up','down','left','right'")
+    dest_peg = peg_destination(origin_peg_x, origin_peg_y, direction[0])
     if not in_bounds(dest_peg[0], dest_peg[1]):
-        raise InvalidMoveExpection("The destination of the move is out of bounds")
+        raise InvalidMoveExpection(
+                "The destination of the move is out of bounds")
+
 
 def letter_to_index(letter):
     ''' Changes 'a'..'g' into '0..6' '''
@@ -29,14 +33,16 @@ def letter_to_index(letter):
     # 97 is 'a' in ascii table
     return ord(letter)-97
 
+
 def in_bounds(position_x, position_y):
     return (position_x >= 0 and position_x < BOARD_SIZE and
             position_y >= 0 and position_y < BOARD_SIZE)
 
+
 def peg_destination(origin_x, origin_y, direction):
-    dict_move = { "u": (0, -2),  # up
-                  "d": (0, +2),  # down
-                  "l": (-2, 0),  # left
-                  "r": (+2, 0) } # right
-    move=dict_move[direction]
+    dict_move = {"u": (0, -2),  # up
+                 "d": (0, +2),  # down
+                 "l": (-2, 0),  # left
+                 "r": (+2, 0)}  # right
+    move = dict_move[direction]
     return (origin_x+move[0], origin_y+move[1])
--- test_logic.py
+++ test_logic.py
@@ -4,6 +4,7 @@ import unittest
 from gamelogic import letter_to_index, in_bounds, move, InvalidMoveExpection
 from gamelogic import peg_destination
 
+
 class TestLogic(unittest.TestCase):
 
     def test_letter_to_index(self):
@@ -12,8 +13,8 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(letter_to_index("g"), 6)
 
     def test_in_bounds(self):
-        self.assertTrue(in_bounds(0,0))
-        self.assertFalse(in_bounds(7,0))
+        self.assertTrue(in_bounds(0, 0))
+        self.assertFalse(in_bounds(7, 0))
 
     def test_origin_peg_in_bounds(self):
         with self.assertRaises(InvalidMoveExpection):
@@ -36,11 +37,10 @@ class TestLogic(unittest.TestCase):
             move(None, ("a1", "k"))
 
     def test_peg_destination(self):
-        self.assertEqual((3,3), peg_destination(3, 5, "u"))
-        self.assertEqual((3,3), peg_destination(3, 1, "d"))
-        self.assertEqual((3,3), peg_destination(5, 3, "l"))
-        self.assertEqual((3,3), peg_destination(1, 3, "r"))
-
+        self.assertEqual((3, 3), peg_destination(3, 5, "u"))
+        self.assertEqual((3, 3), peg_destination(3, 1, "d"))
+        self.assertEqual((3, 3), peg_destination(5, 3, "l"))
+        self.assertEqual((3, 3), peg_destination(1, 3, "r"))
 
 
 if __name__ == '__main__':
