77276f31249bd97e15b99f359113b096ebfdc329
Diogo V. Kersting <diogovk@gmail.com>
2016-04-10 23:34:02 -0300

    Use monospace font in html mail clients

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ from models import Game
 from datetime import datetime, timedelta
 
 
+REMINDER_MESSAGE_PLAIN = """
+It's been a while since your last movement in this game of Peg Solitarie.
+Please finish your game.
+Remember that you can cancel or give up at any time.
+Current board:
+{}
+"""
+REMINDER_MESSAGE_HTML = "<pre>{}</pre>".format(REMINDER_MESSAGE_PLAIN)
+
+BOARD_HTML_TEMPLATE = """
+<pre style="font-family: 'Courier New', Courier, monospace;">{}</pre>
+"""
+
+SENDER='noreply@{}.appspotmail.com'.format(app_identity.get_application_id())
+REMINDER_SUBJECT="Please finish your game"
+
 class NotifyInactiveUsers(webapp2.RequestHandler):
     def get(self):
         """
@@ -14,25 +30,20 @@ class NotifyInactiveUsers(webapp2.RequestHandler):
         for over 12 hours.
         One notification per game.
         """
-        app_id = app_identity.get_application_id()
         twelve_hours_ago = datetime.now() - timedelta(hours=12)
         # Get games with no change in the last 12 hours
         games = Game.query(Game.changed_at < twelve_hours_ago)
         for game in games:
             user = game.user.get()
-            subject = "Please finish your game"
             board = "\n".join(game.board)
-            body = """It's been a while since your last movement in this
-            game of Peg Solitarie.
-            Please finish the game.
-            Remember that you can cancel or give up at any time.
-            Current board:
-                {}
-            """.format(board)
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            board_html = BOARD_HTML_TEMPLATE.format(board)
+            message = mail.EmailMessage(sender=SENDER,
+                                        subject=REMINDER_SUBJECT)
+            message.to = user.email
+            message.body = REMINDER_MESSAGE_PLAIN.format(board)
+            message.html = REMINDER_MESSAGE_HTML.format(board_html)
+            message.send()
+
 
 app = webapp2.WSGIApplication([
     ('/crons/notify_inactive_users', NotifyInactiveUsers)
